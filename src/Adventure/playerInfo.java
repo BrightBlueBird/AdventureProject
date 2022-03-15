48f9f78b3765917b844621b762eeeb17b082cc65
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2022-03-15 09:49:44 +0100

    First commit

---
+++ src/Adventure/playerInfo.java
@@ -0,0 +1,11 @@
+package Adventure;
+
+public class playerInfo {
+  private String nameOfPlayer;
+
+  public void setNameOfPlayer (String nameOfPLayer){
+    this.nameOfPlayer = nameOfPLayer;
+  }
+  public String getNameOfPlayer (){
+    return nameOfPlayer;}
+}
